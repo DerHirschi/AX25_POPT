84a1abbc77eedd5ff801db300c06d6acca852f47
masto <hirschi@saw-emusic.de>
2023-01-29 01:14:41 +0100

    Some test with tkinter raw GUI design ..

--- gui/guiMaín.py
+++ gui/guiMaín.py
@@ -1,6 +1,6 @@
 import time
 import tkinter as tk
-from tkinter import scrolledtext, Label, Menu
+from tkinter import scrolledtext, Label, Menu, LEFT
 import threading
 import logging
 from ax25.ax25PortHandler import DevDirewolf, MYHEARD, AX25Conn
@@ -27,16 +27,24 @@ class TkMainWin:
         self.win = tk.Tk()
         # self.mh_win: MHWin
         self.debug_win = None       # TODO .. Class
+        self.new_conn_win = None
         self.win.title("Py AX.25")
         self.win.geometry("1400x600")
         self.win.columnconfigure(1, minsize=500, weight=2)
         self.win.columnconfigure(2, minsize=200, weight=1)
+        self.win.rowconfigure(0, minsize=150, weight=1)
+        self.win.rowconfigure(1, minsize=300, weight=1)
+        self.win.rowconfigure(2, minsize=30, weight=1)
+        self.win.rowconfigure(3, minsize=300, weight=1)
+
+
         ##############
         # Menüleiste
         self.menubar = Menu(self.win)
         self.win.config(menu=self.menubar)
         # Menü 1 "Verbindungen"
         self.MenuVerb = Menu(self.menubar)
+        self.MenuVerb.add_command(label="Neu", command=self.open_new_conn_win)
         self.MenuVerb.add_command(label="Quit", command=self.win.quit)
         self.menubar.add_cascade(label="Verbindungen", menu=self.MenuVerb)
         # Menü 2 "MH"
@@ -44,27 +52,85 @@ class TkMainWin:
         self.menubar.add_command(label="MH", command=self.MH_win)
         # Menü 3 "Tools"
         self.MenuTools = Menu(self.menubar)
-        self.MenuTools.add_command(label="Debug Tool", command=self.DEBUG_win)
+        # self.MenuTools.add_command(label="Debug Tool", command=self.DEBUG_win)
         self.menubar.add_cascade(label="Tools", menu=self.MenuTools)
+        # Menü 4 "Debug"
+        self.menubar.add_command(label="Debug", command=self.DEBUG_win)
         ##############
         # Textfenster
         # Vorschreibfenster
         self.inp_txt = tk.Text(self.win, background='black', foreground='yellow', font=("TkFixedFont", TEXT_SIZE))
-        self.win.rowconfigure(0, minsize=150, weight=1)
         self.inp_txt.grid(row=0, column=1, sticky="nsew")
         # Ausgabe
         self.out_txt = tk.Text(self.win, background='black', foreground='red', font=("TkFixedFont", TEXT_SIZE))
-        self.win.rowconfigure(1, minsize=300, weight=1)
         self.out_txt.grid(row=1, column=1, sticky="nsew")
+        # CH Buttons
+        self.ch_btn_frame = tk.Frame(self.win, width=500, height=30)
+        self.ch_btn_frame.columnconfigure(1, minsize=50, weight=1)
+        self.ch_btn_frame.columnconfigure(2, minsize=50, weight=1)
+        self.ch_btn_frame.columnconfigure(3, minsize=50, weight=1)
+        self.ch_btn_frame.columnconfigure(4, minsize=50, weight=1)
+        self.ch_btn_frame.columnconfigure(5, minsize=50, weight=1)
+        self.ch_btn_frame.columnconfigure(6, minsize=50, weight=1)
+        self.ch_btn_frame.columnconfigure(7, minsize=50, weight=1)
+        self.ch_btn_frame.columnconfigure(8, minsize=50, weight=1)
+        self.ch_btn_frame.columnconfigure(9, minsize=50, weight=1)
+        self.ch_btn_frame.columnconfigure(10, minsize=50, weight=1)
+        self.ch_btn_frame.grid(row=2, column=1, sticky="nsew")
+        self.ch_button1 = tk.Button(self.ch_btn_frame, text=" 1 ", bg="green")
+        self.ch_button2 = tk.Button(self.ch_btn_frame, text=" 2 ", bg="red")
+        self.ch_button3 = tk.Button(self.ch_btn_frame, text=" 3 ", bg="red")
+        self.ch_button4 = tk.Button(self.ch_btn_frame, text=" 4 ", bg="red")
+        self.ch_button5 = tk.Button(self.ch_btn_frame, text=" 5 ", bg="red")
+        self.ch_button6 = tk.Button(self.ch_btn_frame, text=" 6 ", bg="red")
+        self.ch_button7 = tk.Button(self.ch_btn_frame, text=" 7 ", bg="red")
+        self.ch_button8 = tk.Button(self.ch_btn_frame, text=" 8 ", bg="red")
+        self.ch_button9 = tk.Button(self.ch_btn_frame, text=" 9 ", bg="red")
+        self.ch_button10 = tk.Button(self.ch_btn_frame, text=" 10 ", bg="red")
+        self.ch_button1.grid(row=1, column=1, sticky="nsew")
+        self.ch_button2.grid(row=1, column=2, sticky="nsew")
+        self.ch_button3.grid(row=1, column=3, sticky="nsew")
+        self.ch_button4.grid(row=1, column=4, sticky="nsew")
+        self.ch_button5.grid(row=1, column=5, sticky="nsew")
+        self.ch_button6.grid(row=1, column=6, sticky="nsew")
+        self.ch_button7.grid(row=1, column=7, sticky="nsew")
+        self.ch_button8.grid(row=1, column=8, sticky="nsew")
+        self.ch_button9.grid(row=1, column=9, sticky="nsew")
+        self.ch_button10.grid(row=1, column=10, sticky="nsew")
+
         # Monitor
         self.mon_txt = scrolledtext.ScrolledText(self.win, background='black', foreground='green', font=("TkFixedFont", TEXT_SIZE))
-        self.win.rowconfigure(2, minsize=300, weight=1)
         # frame.grid(column=0, row=0, columnspan=3, rowspan=2)
-        self.mon_txt.grid(row=2, column=1, columnspan=2, sticky="nsew")
+        self.mon_txt.grid(row=3, column=1, columnspan=2, sticky="nsew")
         #######################
         # SeitenLabel ( TEST )
-        self.test_lable = Label(self.win, text="", font=("Arial", 15))
-        self.test_lable.grid(row=1, column=2)
+        self.side_frame = tk.Frame(self.win, width=500, height=30)
+        self.side_frame.rowconfigure(0, minsize=100, weight=1)
+        self.side_frame.rowconfigure(1, minsize=400, weight=1)
+        self.side_frame.grid(row=0, column=2, rowspan=2, sticky="nsew")
+        # Seiten Buttons
+        self.side_btn_frame = tk.Frame(self.side_frame, width=500, height=30)
+        self.side_btn_frame.rowconfigure(0, minsize=100, weight=1)
+        self.side_btn_frame.rowconfigure(1, minsize=400, weight=1)
+        self.side_btn_frame.columnconfigure(0, minsize=100, weight=1)
+        self.side_btn_frame.columnconfigure(1, minsize=100, weight=1)
+        self.side_btn_frame.columnconfigure(2, minsize=100, weight=1)
+        self.side_btn_frame.columnconfigure(3, minsize=100, weight=1)
+        self.side_btn_frame.grid(row=0, column=0, sticky="nsew")
+        self.side_btn_frame.rowconfigure(0, minsize=50, weight=1)
+        self.side_btn_frame.rowconfigure(1, minsize=50, weight=1)
+        # New Conncetion
+        self.conn_btn = tk.Button(self.side_btn_frame, text="New Conn", bg="green", command=self.open_new_conn_win)
+        self.conn_btn.grid(row=0, column=0, sticky="nsew")
+        self.test_btn = tk.Button(self.side_btn_frame, text="Test", bg="yellow")
+        self.test_btn.grid(row=1, column=0, sticky="nsew")
+        self.test_btn1 = tk.Button(self.side_btn_frame, text="Test2", bg="red")
+        self.test_btn1.grid(row=1, column=1, sticky="nsew")
+
+        self.test_lable = Label(self.side_frame, text="1\n2\n3\n4\n5\n6\n7\n8\n", font=("Arial", 15))
+        self.test_lable.grid(row=1, column=0)
+        #self.test_lable1 = Label(self.side_frame, text="1\n2\n3\n4\n5\n6\n7\n8\n", font=("Arial", 15))
+        #self.test_lable1.grid(row=1, column=0)
 
         #######################
         # LOOP
@@ -77,6 +143,13 @@ class TkMainWin:
             self.ax25_ports_th.start()
         self.update_mon()
         self.update_status_win()
+        if self.debug_win is not None:
+            self.debug_win: DEBUGwin
+            if self.debug_win.is_running:
+                self.debug_win.tasker()
+            else:
+                # self.debug_win.close()
+                self.debug_win = None
         self.win.after(LOOP_DELAY, self.tasker)
 
     #################
@@ -152,6 +225,37 @@ class TkMainWin:
         self.axtest_port.run_once()
 
     ##############
+    # New Connection WIN
+    def open_new_conn_win(self):
+        # TODO
+        if self.new_conn_win is None:
+            self.new_conn_win = tk.Tk()
+            self.new_conn_win.title("New Connection")
+            self.new_conn_win.geometry("700x300")
+            self.new_conn_win.protocol("WM_DELETE_WINDOW", self.destroy_new_conn_win)
+            self.new_conn_win.columnconfigure(0, minsize=200, weight=2)
+            self.new_conn_win.columnconfigure(1, minsize=200, weight=1)
+            self.new_conn_win.columnconfigure(3, minsize=300, weight=1)
+            self.new_conn_win.rowconfigure(0, minsize=100, weight=1)
+            self.new_conn_win.rowconfigure(1, minsize=100, weight=1)
+            self.new_conn_win.rowconfigure(2, minsize=50, weight=1)
+            self.new_conn_win.rowconfigure(3, minsize=50, weight=1)
+
+            call_txt_inp = tk.Text(self.win, background='black', foreground='yellow', font=("TkFixedFont", TEXT_SIZE))
+            call_txt_inp.grid(row=0, column=1, sticky="nsew")
+
+            conn_btn = tk.Button(self.new_conn_win, text="Los", bg="green", command=self.open_new_conn_win)
+            conn_btn.grid(row=3, column=0, sticky="nsew")
+
+
+    def destroy_new_conn_win(self):
+        self.new_conn_win.destroy()
+        self.new_conn_win = None
+
+    # New Connection WIN
+    # ##############
+
+    ##############
     # MH WIN
     def MH_win(self):
         """MH WIN"""
@@ -161,8 +265,12 @@ class TkMainWin:
 
     ##############
     # DEBUG WIN
-    def DEBUG_win(self, ):
-        DEBUGwin(self.axtest_port.connections)
+    def DEBUG_win(self):
+        if self.debug_win is None:
+            self.debug_win = DEBUGwin(self.axtest_port.connections)
+        else:
+            if not self.debug_win.is_running:
+                self.debug_win = DEBUGwin(self.axtest_port.connections)
 
     # DEBUG WIN ENDE
     ##############
