f669a87f79d60b119e8118cff8495c832848fe55
masto <hirschi@saw-emusic.de>
2023-04-14 19:24:46 +0200

    Fixed: AX25Frame Encoding error when outgoing connection with own call SSID

--- config_station.py
+++ config_station.py
@@ -3,7 +3,7 @@ import os
 from cli.cli import DefaultCLI, NoneCLI
 from ax25.ax25UI_Pipe import AX25Pipe
 
-VER = '2.76.2dev'
+VER = '2.76.3dev'
 
 CFG_data_path = 'data/'
 CFG_usertxt_path = 'userdata/'
