24f4c241a007c131fa85508b991dff0880c17d79
masto <hirschi@saw-emusic.de>
2024-02-04 19:39:13 +0100

    - CLI "PORT" CMD is showing Dual-Port

--- cli/cliMain.py
+++ cli/cliMain.py
@@ -1069,24 +1069,29 @@ class DefaultCLI(object):
             port = self.port_handler.ax25_ports[port_id]
             name = str(port.portname).ljust(7)
             typ = port.port_typ.ljust(7)
-            stations = port.my_stations
-            if not stations:
-                stations = ['']
-            digi = ''
+            if port.dualPort_primaryPort in [port, None]:
 
-            if stations[0] in port.stupid_digi_calls and stations[0]:
-                digi = '(DIGI)'
-            if stations[0] in port.port_cfg.parm_cli.keys():
-                digi = f"{port.port_cfg.parm_cli[stations[0]].ljust(7)} " + digi
-
-            ret += f" {str(port_id).ljust(2)} {name} {typ}  {stations[0].ljust(9)} {digi}\r"
-            for stat in stations[1:]:
+                stations = port.my_stations
+                if not stations:
+                    stations = ['']
                 digi = ''
-                if stat in port.stupid_digi_calls:
+
+                if stations[0] in port.stupid_digi_calls and stations[0]:
                     digi = '(DIGI)'
-                if stat in port.port_cfg.parm_cli.keys():
-                    digi = f"{port.port_cfg.parm_cli[stat].ljust(7)} " + digi
-                ret += f"                     {stat.ljust(9)} {digi}\r"
+                if stations[0] in port.port_cfg.parm_cli.keys():
+                    digi = f"{port.port_cfg.parm_cli[stations[0]].ljust(7)} " + digi
+
+                ret += f" {str(port_id).ljust(2)} {name} {typ}  {stations[0].ljust(9)} {digi}\r"
+                for stat in stations[1:]:
+                    digi = ''
+                    if stat in port.stupid_digi_calls:
+                        digi = '(DIGI)'
+                    if stat in port.port_cfg.parm_cli.keys():
+                        digi = f"{port.port_cfg.parm_cli[stat].ljust(7)} " + digi
+                    ret += f"                     {stat.ljust(9)} {digi}\r"
+            else:
+                if port.dualPort_primaryPort:
+                    ret += f" {str(port_id).ljust(2)} {name} {typ}  Dual-Port: Secondary from Port {port.dualPort_primaryPort.port_id} \r"
         ret += '\r'
         return ret
 
